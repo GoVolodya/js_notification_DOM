caba2ec95d930ec73ec62ab2a12a3f66c1d1b762
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-30 11:05:48 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const block = document.createElement("div");
+    const heading = document.createElement("h2");
+    const message = document.createElement("p");
+    heading.classList.add("title");
+    heading.textContent = title;
+    message.textContent = description;
+    block.classList.add("notification", type);
+    block.style.top = `${posTop}px`;
+    block.style.right = `${posRight}px`;
+    block.append(heading, message);
+    document.body.append(block);
+    setTimeout(()=>block.style.display = "none", 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
